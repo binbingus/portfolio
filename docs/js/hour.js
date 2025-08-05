f9e95cfe228d53bd7cd9364f1e9965add3599232
uo288066 <uo288066@uniovi.es>
2025-08-05 13:58:08 +0200

    Better grid and navbar

---
+++ docs/js/hour.js
@@ -0,0 +1,8 @@
+function updateClock() {
+    const now = new Date();
+    const time = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+    document.getElementById("clock").textContent = time;
+}
+
+updateClock(); // initial update
+setInterval(updateClock, 1000); // update every second
