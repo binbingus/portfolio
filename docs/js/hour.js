e13615ee282cb2ee93079a2088bc231df242eb7d
uo288066 <uo288066@uniovi.es>
2025-08-05 16:54:08 +0200

    phone view navbar finished

--- docs/js/hour.js
+++ docs/js/hour.js
@@ -1,6 +1,6 @@
 function updateClock() {
     const now = new Date();
-    const time = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+    const time = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false });
     document.getElementById("clock").textContent = time;
 }
 
