479081a3cb6e5bc51bc2a9832fb761cc952fab3e
uo288066 <uo288066@uniovi.es>
2025-08-26 19:37:13 +0200

    idk

--- docs/js/loader.js
+++ docs/js/loader.js
@@ -1,11 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
     const btn = document.getElementById('loading-entry');
     const loader = document.getElementById('loader');
+    const href = btn ? btn.getAttribute('href') : null;
     if (btn && loader) {
         btn.addEventListener('click', function() {
+            event.preventDefault();
             loader.style.visibility = 'visible'; // Show the loader
             setTimeout(() => {
-                window.location.href = 'desktop.html';
+                if (href) {
+                    window.location.href = href; // Navigate to the specified href
+                } else {    
+                    window.location.href = 'desktop.html';
+                }
             }, 1000);
         });
     }
